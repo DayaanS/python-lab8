06c22b429fa5a80ae3840c3b889165ac841d9d09
Dayaana <daiaanasemyonova@yandex.ru>
2026-03-31 17:56:50 +0300

    task2 side counter + color and size vars

--- task2.py
+++ task2.py
@@ -5,6 +5,18 @@ bl = 5
 th1 = 100
 th2 = 250
 
+# colors 
+clr_line = (0,0,255)
+clr_outl = (0,0,255)
+clr_cent = (0,0,255)
+clr_txt = (0,0,0)
+font = cv2.FONT_HERSHEY_SIMPLEX
+thick1 = 2
+
+on_left = False
+left_count = 0
+right_count = 0
+
 capture = cv2.VideoCapture(0)
 
 while True:
@@ -24,15 +36,27 @@ while True:
         param1=100,
         param2=40,
         minRadius=1,
-        maxRadius=60 #
+        maxRadius=60
     )
     if circles is not None:
         circles = np.uint16(np.around(circles))
         x,y,r = circles[0][0]
-        cv2.circle(output, (x,y), r, (0,0,255),2) #outline
-        cv2.circle(output, (x,y), 2, (0,0,255), 3) #center
+        cv2.circle(output, (x,y), r, clr_outl, thick1) #outline
+        cv2.circle(output, (x,y), 2, clr_cent, thick1) #center
+        
+        if x < (w // 2): 
+            if on_left == False:
+                left_count += 1
+                on_left = True
+        else:
+            if on_left == True:
+                right_count += 1
+                on_left = False
 
-    cv2.line(output, (w // 2, 0), (w // 2, h), (0,0,255), 2)
+    cv2.line(output, (w // 2, 0), (w // 2, h), clr_line, thick1)
+    cv2.putText(output, str(left_count), (100, 100), font, 1, clr_txt, thick1)
+    cv2.putText(output, str(right_count), (w-100, 100), font, 1, clr_txt, thick1)
+    
 
     cv2.imshow('circle', output)
     
