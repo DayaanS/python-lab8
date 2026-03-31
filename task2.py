4187b3fa46c1392fa04886d4b47d92492462639d
Dayaana <daiaanasemyonova@yandex.ru>
2026-03-31 19:54:27 +0300

    separe func for circles

--- task2.py
+++ task2.py
@@ -9,7 +9,7 @@ th2 = 250
 clr_line = (0,0,255)
 clr_outl = (0,0,255)
 clr_cent = (0,0,255)
-clr_txt = (0,0,0)
+clr_txt = (0,0,255)
 font = cv2.FONT_HERSHEY_SIMPLEX
 thick1 = 2
 
@@ -19,7 +19,7 @@ right_count = 0
 
 capture = cv2.VideoCapture(0)
 fly = cv2.imread('fly64.png', cv2.IMREAD_UNCHANGED)
-fh, fw, fc = fly.shape
+
 
 def overlay(background, overlay, x, y):
     oh, ow, _ = overlay.shape
@@ -30,19 +30,9 @@ def overlay(background, overlay, x, y):
         background[y-oh//2:y+oh//2, x-ow//2:x+ow//2, c] = (overlay_alpha * overlay[:, :, c] + background_alpha * background[y-oh//2:y+oh//2, x-ow//2:x+ow//2, c])
     return background
 
-
-while True:
-    
-    error, frame = capture.read()
-    output = frame.copy()
-    h, w, _ = output.shape
-
-    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    img_blur = cv2.blur(img_gray, (bl,bl))
-
-
+def detect_circles(img):
     circles = cv2.HoughCircles(
-        img_blur,
+        img,
         cv2.HOUGH_GRADIENT,
         dp=1,
         minDist=100,
@@ -51,12 +41,29 @@ while True:
         minRadius=1,
         maxRadius=60
     )
+
     if circles is not None:
         circles = np.uint16(np.around(circles))
-        x,y,r = circles[0][0]
-        cv2.circle(output, (x,y), r, clr_outl, thick1) #outline
-        cv2.circle(output, (x,y), 2, clr_cent, thick1) #center
-        
+        x,y,r = circles[0][0] 
+        cv2.circle(output, (x,y), r, clr_outl, thick1) # draw outline
+        overlay(output, fly, x, y)
+        return x # x coordinate of the circle for counter
+
+while True:
+    
+    error, frame = capture.read()
+    output = frame.copy() 
+    h, w, _ = output.shape
+
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    img_blur = cv2.blur(img_gray, (bl,bl))
+
+    cv2.line(output, (w // 2, 0), (w // 2, h), clr_line, thick1)
+    cv2.putText(output, str(left_count), (100, 100), font, 1, clr_txt, thick1)
+    cv2.putText(output, str(right_count), (w-100, 100), font, 1, clr_txt, thick1)
+    
+    x = detect_circles(img_blur)
+    if x:
         if x < (w // 2): 
             if on_left == False:
                 left_count += 1
@@ -66,15 +73,7 @@ while True:
                 right_count += 1
                 on_left = False
 
-        overlay(output, fly, x, y)
-
-    cv2.line(output, (w // 2, 0), (w // 2, h), clr_line, thick1)
-    cv2.putText(output, str(left_count), (100, 100), font, 1, clr_txt, thick1)
-    cv2.putText(output, str(right_count), (w-100, 100), font, 1, clr_txt, thick1)
-    
-    
-
-    cv2.imshow('circle', output)
+    cv2.imshow('circle detector', output)
     
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
